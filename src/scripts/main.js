9a1983270dda7647843dbd52303e6cad07a03753
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-10-02 11:28:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function startVideo(event) {
+  const playButton = event.target;
+
+  playButton.style.visibility = 'hidden';
+  playButton.closest('#videoblock__poster').style.visibility = 'hidden';
+  playButton.closest('#videoblock__overlay').style.visibility = 'hidden';
+  playButton.closest('#presentation-section').firstElementChild.play();
+}
+
+function stopVideo(event) {
+  const video = event.target;
+
+  video.pause();
+  document.querySelector('#videoblock__overlay').style.visibility = 'visible';
+  document.querySelector('.videoblock__button').style.visibility = 'visible';
+}
+
+document.addEventListener('click', (event) => {
+  if (event.target.className === 'videoblock__button') {
+    startVideo(event);
+  }
+
+  if (event.target.className === 'videoblock__container') {
+    stopVideo(event);
+  }
+});
